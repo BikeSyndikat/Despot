c1867c0548dd4919235f6738bfffc8f915d28cf8
Norbert <4git.20.elbrecht@spamgourmet.com>
2014-01-01 15:09:28 +0100

    Add database setup script to define the views to work on. These do not include privilege or ownership information.

---
+++ src/main/sql/setup.sql
@@ -0,0 +1,44 @@
+DROP VIEW IF EXISTS workorder;
+CREATE OR REPLACE VIEW workorder AS
+ SELECT "Auftragsannahme"."Id" AS id,
+        date_part('day'::text, "Auftragsannahme"."Dat") AS dat,
+        "Auftragsannahme"."Uhr" AS uhr,
+        "Auftragsannahme"."Von" AS von,
+        "Auftragsannahme"."Ueber" AS ueber,
+        "Auftragsannahme"."Nach" AS nach,
+        "Auftragsannahme"."Job" AS job,
+        "Auftragsannahme"."FahrscheckNr" AS fahrschecknr,
+        "Auftragsannahme"."Preis" AS preis,
+        "Auftragsannahme"."FahrerNr" AS fahrernr,
+        "Auftragsannahme"."Typ" AS typ,
+
+        -- possibly redundant:
+        "Auftragsannahme"."PreisCheck" AS preischeck,
+        "Auftragsannahme"."FahrscheckCheck" AS fahrscheckcheck,
+        "Auftragsannahme"."Sonderkonditionen" AS sonderkonditionen,
+        "Auftragsannahme"."Summe" AS summe,
+        "Auftragsannahme"."Kuerzel" AS kuerzel,
+
+        -- info from joined table(s):
+        "Rechnungskunden"."Firmierung1" AS firmierung1
+
+   FROM "Auftragsannahme"
+   JOIN "Rechnungskunden" ON "Auftragsannahme"."Kuerzel" = "Rechnungskunden"."ID"
+  WHERE "Auftragsannahme"."Dat" >= (date_trunc('month'::text, now()) - '1 mon'::interval)
+  ORDER BY "Auftragsannahme"."Dat" DESC;
+
+DROP VIEW IF EXISTS customer;
+CREATE OR REPLACE VIEW customer AS
+ SELECT "Rechnungskunden"."ID" AS id,
+        "Rechnungskunden"."Kuerzel" AS kuerzel,
+        "Rechnungskunden"."Firmierung1" AS firmierung1,
+        "Rechnungskunden"."Firmierung2" AS firmierung2,
+        "Rechnungskunden"."Straße" AS strasse,
+        "Rechnungskunden"."PLZ" AS plz,
+        "Rechnungskunden"."Ort" AS ort,
+        "Rechnungskunden"."Kd-Nr" AS kundennummer,
+        "Rechnungskunden"."Telefon" AS telefon,
+        "Rechnungskunden"."Durchwahl" AS durchwahl
+
+   FROM "Rechnungskunden"
+  ORDER BY "Rechnungskunden"."Kuerzel" ASC;
