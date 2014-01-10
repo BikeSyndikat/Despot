072f63563404dc01fb71d4e8e99c888a54709fda
Norbert <4git.20.elbrecht@spamgourmet.com>
2014-01-11 00:01:01 +0100

    Push down refactoring putting specialized find methods to the workorder facade. These will refresh the entities with changes from the database.

--- src/main/java/org/ifbma/legacy/despot/jsfbeans/AbstractFacade.java
+++ src/main/java/org/ifbma/legacy/despot/jsfbeans/AbstractFacade.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.ifbma.legacy.despot.jsfbeans;
 
 import java.util.List;
@@ -11,12 +5,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaQuery;
 
-/**
- *
- * @author norbert
- */
 public abstract class AbstractFacade<T> {
-    private Class<T> entityClass;
+    protected Class<T> entityClass;
 
     public AbstractFacade(Class<T> entityClass) {
         this.entityClass = entityClass;
--- src/main/java/org/ifbma/legacy/despot/jsfbeans/WorkorderFacade.java
+++ src/main/java/org/ifbma/legacy/despot/jsfbeans/WorkorderFacade.java
@@ -1,12 +1,21 @@
 package org.ifbma.legacy.despot.jsfbeans;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
 import org.ifbma.legacy.despot.entities.Workorder;
 
 @Stateless
 public class WorkorderFacade extends AbstractFacade<Workorder> {
+
+    private static final Logger LOG = Logger.getLogger(WorkorderFacade.class.getCanonicalName());
+
     @PersistenceContext(unitName = "LegacyDespot_1_PU")
     private EntityManager em;
 
@@ -19,4 +28,68 @@ public class WorkorderFacade extends AbstractFacade<Workorder> {
         super(Workorder.class);
     }
 
+    /**
+     * Find range method that explicitly reads in values from the database.
+     * <p>
+     * @param range
+     * @return
+     */
+    @Override
+    public List<Workorder> findRange(int[] range) {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        q.setHint("javax.persistence.cache.storeMode", "REFRESH");
+        return q.getResultList();
+    }
+
+    /**
+     * Find range method that goes through the result list that has every
+     * (hopefully few) entity refreshed that meets certain criteria.
+     * <p>
+     * Right here we expect the brand new entities be interesting for just one
+     * change of internal state we do not want to miss.
+     * <p>
+     * @param range
+     * @return
+     */
+    @Override
+    public List<Workorder> findNewestRange(int[] range) {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        TypedQuery<Workorder> q = getEntityManager()
+                .createNamedQuery(entityClass.getSimpleName() + ".findAll", entityClass);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        refresh(q.getResultList());
+        return q.getResultList();
+    }
+
+    /**
+     * Method to call on return list of query, like the find* methods. This
+     * will filter and refresh entities. If there are very few entities
+     * expected to be refreshed by criteria this method is useful.
+     * <p>
+     * @param resultList
+     */
+    private void refresh(List<Workorder> resultList) {
+        for (Workorder workorder : resultList) {
+            if (workorder.getFahrernr() < 2) {
+                LOG.log(Level.INFO, "refreshing workorder {0}", workorder.getId());
+                getEntityManager().refresh(workorder);
+            }
+        }
+    }
+
+    public void testLogLevel() {
+        LOG.finest(Level.FINEST.getLocalizedName());
+        LOG.finer(Level.FINER.getLocalizedName());
+        LOG.fine(Level.FINE.getLocalizedName());
+        LOG.config(Level.CONFIG.getLocalizedName());
+        LOG.info(Level.INFO.getLocalizedName());
+        LOG.warning(Level.WARNING.getLocalizedName());
+        LOG.severe(Level.SEVERE.getLocalizedName());
+    }
 }
