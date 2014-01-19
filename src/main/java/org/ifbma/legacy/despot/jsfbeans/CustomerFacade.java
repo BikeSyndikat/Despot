db825bec5debf2765f6f0f1fdaf6e63ec12def96
Norbert <4git.20.elbrecht@spamgourmet.com>
2014-01-19 18:54:08 +0100

    Add methods for search and count to the customer facade/DAO that operate with filters and sorting. The logic is tied to Primefaces, though.

--- src/main/java/org/ifbma/legacy/despot/jsfbeans/CustomerFacade.java
+++ src/main/java/org/ifbma/legacy/despot/jsfbeans/CustomerFacade.java
@@ -1,12 +1,16 @@
 package org.ifbma.legacy.despot.jsfbeans;
 
+import java.util.List;
+import java.util.Map;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
+import javax.persistence.criteria.*;
 import org.ifbma.legacy.despot.entities.Customer;
+import org.primefaces.model.SortOrder;
 
 @Stateless
 public class CustomerFacade extends AbstractFacade<Customer> {
+
     @PersistenceContext(unitName = "LegacyDespot_1_PU")
     private EntityManager em;
 
@@ -17,6 +21,118 @@ public class CustomerFacade extends AbstractFacade<Customer> {
 
     public CustomerFacade() {
         super(Customer.class);
+    }
+
+    /**
+     * Count according to filter expression.
+     * <p/>
+     * @param filters
+     * @return
+     */
+    public int count(Map<String, String> filters) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery();
+        Root<Customer> rt = cq.from(entityClass);
+        if (filters != null && filters.size() > 0) {
+            Expression p = getFilterCondition(cb, rt, filters);
+            if (p != null) {
+                cq.where(p);
+            }
+        }
+
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult()).intValue();
+
+    }
+
+    /**
+     * Find range according to filter expression.
+     * <p/>
+     * @param range
+     * @param sortField
+     * @param sortOrder
+     * @param filters
+     * @return
+     */
+    public List<Customer> findRange(int[] range, String sortField,
+            final SortOrder sortOrder, Map<String, String> filters) {
+
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+
+        CriteriaQuery cq = cb.createQuery();
+        final Root<Customer> rootQuery = cq.from(entityClass);
+
+        cq.select(rootQuery);
+        if (sortField != null) {
+            Order sortExpression = getSortExpression(sortOrder, cb, rootQuery, sortField);
+            cq.orderBy(sortExpression);
+        }
+
+        if (filters != null && filters.size() > 0) {
+            Expression p = getFilterCondition(cb, rootQuery, filters);
+            if (p != null) {
+                cq.where(p);
+            }
+        }
+
+        Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+
+    /**
+     * Create a sort expression out of primefaces sorting parameters. Used for
+     * ajax calls from primefaces jsf tags.
+     * <p/>
+     * @param sortOrder
+     * @param cb
+     * @param rootQuery
+     * @param sortField
+     * @return
+     */
+    protected Order getSortExpression(final SortOrder sortOrder,
+            CriteriaBuilder cb, final Root<Customer> rootQuery, String sortField) {
+
+        final Order sortExpression;
+        if (sortOrder.equals(SortOrder.ASCENDING)) {
+            sortExpression = cb.asc(rootQuery.get(sortField));
+        } else {
+            sortExpression = cb.desc(rootQuery.get(sortField));
+        }
+        return sortExpression;
+    }
+
+    /**
+     * Get filter conditions. Does seed out globalFilter as a reserved
+     * primefaces identifier used with ajax.
+     * <p/>
+     * @param cb
+     * @param myObj
+     * @param filters
+     * @return
+     */
+    protected Expression getFilterCondition(CriteriaBuilder cb, Root<Customer> myObj, Map<String, String> filters) {
+        Expression<Boolean> filterCondition = null;
+        final String WILDCARD = "%";
+
+        for (Map.Entry<String, String> filter : filters.entrySet()) {
+            String value = WILDCARD + filter.getValue() + WILDCARD;
+            if (!filter.getValue().isEmpty()) {
+                if (!filter.getKey().equals("globalFilter")) {
+                    Path<String> path = myObj.get(filter.getKey());
+                    Predicate newStatement = cb.like(path, value);
+
+                    if (filterCondition == null) {
+                        filterCondition = newStatement;
+                    } else {
+                        filterCondition = cb.or(filterCondition, newStatement);
+                    }
+                }
+            }
+        }
+        return filterCondition;
     }
 
 }
